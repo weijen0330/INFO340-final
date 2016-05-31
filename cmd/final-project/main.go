2b529d60c6889c80b20417d60c32874247cf6074
Wei-Jen <weijen0330@hotmail.com>
2016-05-31 16:08:36 -0700

    initial commit

---
+++ cmd/final-project/main.go
@@ -0,0 +1,211 @@
+// by setting package as main, Go will compile this as an executable file.
+// Any other package turns this into a library
+package main
+
+// These are your imports / libraries / frameworks
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+var (
+	// this is the pointer to the database we will be working with
+	// this is a "global" variable (sorta kinda, but you can use it as such)
+	db *sql.DB
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	var errd error
+	// here we want to open a connection to the database using an environemnt variable.
+	// This isn't the best technique, but it is the simplest one for heroku
+	db, errd = sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if errd != nil {
+		log.Fatalf("Error opening database: %q", errd)
+	}
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("html/*")
+	router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	router.GET("/ping", func(c *gin.Context) {
+		ping := db.Ping()
+		if ping != nil {
+			// our site can't handle http status codes, but I'll still put them in cause why not
+			c.JSON(http.StatusOK, gin.H{"error": "true", "message": "db was not created. Check your DATABASE_URL"})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"error": "false", "message": "db created"})
+		}
+	})
+
+	router.GET("/query1", func(c *gin.Context) {
+		table := "<table class='table'><thead><tr>"
+		// put your query here
+		rows, err := db.Query("SELECT SUM(length) FROM song WHERE albumId IN (SELECT albumId FROM album WHERE cost < '10.00');") // <--- EDIT THIS LINE
+		if err != nil {
+			// careful about returning errors to the user!
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		// foreach loop over rows.Columns, using value
+		cols, _ := rows.Columns()
+		if len(cols) == 0 {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+		for _, value := range cols {
+			table += "<th class='text-center'>" + value + "</th>"
+		}
+		// once you've added all the columns in, close the header
+		table += "</thead><tbody>"
+		// declare all your RETURNED columns here  
+		var length int  
+		  // <--- EDIT THESE LINES //<--- ^^^^
+		for rows.Next() {
+			// assign each of them, in order, to the parameters of rows.Scan.
+			// preface each variable with &
+			rows.Scan(&length) // <--- EDIT THIS LINE
+			// can't combine ints and strings in Go. Use strconv.Itoa(int) instead
+			table += "<tr><td>" + strconv.Itoa(length) + "</td></tr>" // <--- EDIT THIS LINE
+		}
+		// finally, close out the body and table
+		table += "</tbody></table>"
+		c.Data(http.StatusOK, "text/html", []byte(table))
+	})
+
+	router.GET("/query2", func(c *gin.Context) {
+		table := "<table class='table'><thead><tr>"
+		// put your query here
+		rows, err := db.Query("SELECT MAX(length) FROM song JOIN album ON song.albumId = album.albumId WHERE genre = 'Pop';") // <--- EDIT THIS LINE
+		if err != nil {
+			// careful about returning errors to the user!
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		// foreach loop over rows.Columns, using value
+		cols, _ := rows.Columns()
+		if len(cols) == 0 {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+		for _, value := range cols {
+			table += "<th class='text-center'>" + value + "</th>"
+		}
+		// once you've added all the columns in, close the header
+		table += "</thead><tbody>"
+		// columns
+		var time int
+
+		for rows.Next() {
+
+			rows.Scan(&time) 
+			table += "<tr><td>" + strconv.Itoa(time) + "</td></tr>" 
+		}
+		// finally, close out the body and table
+		table += "</tbody></table>"
+		c.Data(http.StatusOK, "text/html", []byte(table))
+	}) 
+
+	router.GET("/query3", func(c *gin.Context) {
+		table := "<table class='table'><thead><tr>"
+		// put your query here
+		rows, err := db.Query("SELECT firstName, lastName, album.title, cost FROM artist JOIN album ON artist.artistId = album.artistId WHERE artist.age > 25;") // <--- EDIT THIS LINE
+		if err != nil {
+			// careful about returning errors to the user!
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		// foreach loop over rows.Columns, using value
+		cols, _ := rows.Columns()
+		if len(cols) == 0 {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+		for _, value := range cols {
+			table += "<th class='text-center'>" + value + "</th>"
+		}
+		// once you've added all the columns in, close the header
+		table += "</thead><tbody>"
+		// columns
+		var firstName string
+		var lastName string
+		var title string
+		var cost string
+
+		for rows.Next() {
+			// rows.Scan() // put columns here prefaced with &
+			rows.Scan(&firstName, &lastName, &title, &cost)
+			table += "<tr><td>" + firstName + "</td><td>" + lastName + "</td><td>" + title + "</td><td>" + 
+			cost + "</td></tr>" // <--- EDIT THIS LINE
+		}
+		// finally, close out the body and table
+		table += "</tbody></table>"
+		c.Data(http.StatusOK, "text/html", []byte(table))
+	})
+
+	router.POST("/login", func(c *gin.Context) {
+		// this is meant for SQL injection examples ONLY.
+		// Don't copy this for use in an actual environment, even if you do stop SQL injection
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+
+		rows, err := db.Query("SELECT usr.name FROM usr WHERE usr.name = '" + username + "' AND usr.password = '" + password + "';")
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		cols, _ := rows.Columns()
+		if len(cols) == 0 {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		rowCount := 0
+		var resultUser string
+		for rows.Next() {
+			rows.Scan(&resultUser)
+			rowCount++
+		}
+		// quick way to check if the user logged in properly
+		if rowCount == 0 {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+		// instead of HTML, we are going to return a JSON file
+		c.JSON(http.StatusOK, gin.H{"username": resultUser})
+	})
+
+	// NO code should go after this line. it won't ever reach that point
+	router.Run(":" + port)
+}
+
+/*
+Example of processing a GET request
+
+// this will run whenever someone goes to last-first-lab7.herokuapp.com/EXAMPLE
+router.GET("/EXAMPLE", func(c *gin.Context) {
+    // process stuff
+    // run queries
+    // do math
+    //decide what to return
+    c.JSON(http.StatusOK, gin.H{
+        "key": "value"
+        }) // this returns a JSON file to the requestor
+    // look at https://godoc.org/github.com/gin-gonic/gin to find other return types. JSON will be the most useful for this
+})
+
+*/
---
+++ static/main.js
@@ -0,0 +1,33 @@
+$(function(){
+    $.get("/ping", function(data){
+        if(data.error == "true"){
+            $("#results").prepend("<div class='alert alert-danger'><strong>Error!</strong> "+ data.message +"</div>");
+        }
+    }, "json")
+    /*
+    $.get("/query1", function(data){
+        $("#firstQuery").append(data);
+    }, "html")
+
+    $.get("/query2", function(data){
+        $("#secondQuery").append(data);
+    }, "html")
+
+    $.get("/query3", function(data){
+        $("#thirdQuery").append(data);
+    }, "html")
+*/
+    $("#login").click(function(){
+      $.post("/login", {username: $("#username").val(), password: $("#password").val()})
+        .done(function(data){
+          if(data){
+            console.log(data)
+            $("#result").text("Logged in as: "+data.username);
+          } else {
+            console.log("Failed to log in!")
+            $("#result").text("Username / pasword combination invalid!");
+          }
+        });
+    });
+
+})
